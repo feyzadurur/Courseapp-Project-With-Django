3a1de26a2977c1848b0aa991f6fcc7db5d1d9022
feyzadurur <you@example.com>
2024-07-08 17:02:22 +0300

    Backend/Html sorunları çözüldü.

--- courses/models.py
+++ courses/models.py
@@ -10,10 +10,6 @@ class Category(models.Model):
     def __str__(self):
         return f"{self.name}"
     
-    def save(self,*args,**kwargs):
-        self.slug=slugify(self.slug)
-        super().save(args,kwargs)
-
 
 class Course(models.Model):
     title=models.CharField(max_length=50,null=True)
@@ -21,14 +17,10 @@ class Course(models.Model):
     imageUrl=models.CharField(max_length=50,blank=True)
     date=models.DateField(auto_now=True)
     isActive=models.BooleanField()
-    slug=models.SlugField(default="",blank=True,editable=False,null=False,unique=True,db_index=True)
+    slug=models.SlugField(default="",blank=True,null=False,unique=True,db_index=True)
 
-    #category=models(Category,default=1,on_delete=models.CASCADE,related_name="kurslar")
     categories=models.ManyToManyField(Category)
-    def save(self,*args,**kwargs):
-        self.slug=slugify(self.title)
-        super().save(args,kwargs)
-
+   
     def __str__(self):
         return f"{self.title} {self.date}"
     
--- courses/views.py
+++ courses/views.py
@@ -1,90 +1,42 @@
-from django.shortcuts import render,redirect
-from django.http import HttpResponse, HttpResponseRedirect,HttpResponseNotFound
-from django.urls import reverse
+from django.shortcuts import get_object_or_404, render
+from django.core.paginator import Paginator
 from datetime import date,datetime
-
-
-data = {
-    "programlama" : "programlama kategorisine ait kurslar",
-    "web-gelistirme" : "web-gelistirme kategorisine ait kurslar",
-    "mobil" : "mobil kategorisine ait kurslar",
-}
-
- 
-db= {
-    "courses":[
-        {
-            "title": "Javascript kursu",
-            "description": "Javascript kursu aciklamasi",
-            "imageUrl":"1.jpg",
-            "slug":"javascript-kursu",
-            "date": datetime.now,
-            "isActive":True,
-            "isUpdated" : True
-        },
-        {
-            "title": "Python kursu",
-            "description": "Python kursu aciklamasi",
-            "imageUrl":"2.jpg",
-            "slug":"python-kursu",
-             "date": datetime.now,
-            "isActive":False,
-            "isUpdated" : False
-        },
-        {
-            "title": "Web geliştirme kursu",
-            "description": "Web geliştirme  kursu aciklamasi",
-            "imageUrl":"3.jpg",
-            "slug":"web-gelistirme -kursu",
-             "date": datetime.now,
-            "isActive":True,
-            "isUpdated" : True
-        }
-    ],
-    "categories": [
-        { "id":1 ,"name": "programlama" ,"slug":"programlama"},
-        { "id":2 ,"name": "web geliştirme", "slug":"web-gelistirme" },
-        { "id":3 ,"name": "mobil uygulamalar", "slug":"mobil-uygulamalar" }
-        ]
-}
+from .models import Course,Category
 
 def index(request):
-    kurslar=[course for course in db["courses"] if course["isActive"]==True]
-    kategoriler=db["categories"]
-    
+    kurslar=Course.objects.filter(isActive=1)
+    kategoriler=Category.objects.all()
     
     return render(request,'courses/index.html',{
         'categories': kategoriler,
         'courses': kurslar
     })
-
-
-def details(request,kurs_adi):
-    return HttpResponse(f"{kurs_adi} detay sayfasi")
-
-
-def getCoursesByCategory(request,category_name):
-    #kurslar=Course.objects.filter(category__slug=slug,isActive=True)
-    #kategoriler=Category.objects.all()
     
-    try:
-        category_text=data[category_name]
-        return render(request,'courses/kurslar.html', {
-            'category' :category_name,
-            'category_text':category_text
-        })
-    except:
-        return HttpResponseNotFound("<h1>yanliş kategori seçimi </h1>")
+
+def details(request,slug):
+    course=get_object_or_404(Course, slug=slug)
+    
+    context={
+        'course':course
+    }
+    return render(request, 'courses/details.html',context)
 
 
-
-def getCoursesByCategoryID(request,category_id):
-
-    #1-2-3->
-    category_list=list(data.keys())
-    if((category_id)> len(category_list)):
-        return HttpResponseNotFound("yanliş kategori seçimi")
-    category_name= category_list[category_id - 1]
-
-    redirect_url=reverse('courses_by_category',args=[category_name])
-    return redirect(redirect_url)
+def getCoursesByCategory(request,slug):
+    kurslar=Course.objects.filter(categories__slug=slug,isActive=True).order_by("date")
+    kategoriler=Category.objects.all()
+    
+    paginator=Paginator(kurslar,2)
+    page=request.GET.get('page',1)
+    page_obj=paginator.page(page)
+    
+    print(page_obj.paginator.count)
+    print(page_obj.paginator.num_pages)
+    
+    return render(request,'courses/index.html',{
+        'categories': kategoriler,
+        'page_obj': page_obj,
+        'seciliKategori':slug
+    })
+    
+ 
